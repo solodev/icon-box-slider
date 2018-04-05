17c178df7a3baf09a7be87b29e5273e053fc393e
Scott Madara <smmadara@gmail.com>
2018-04-05 12:51:45 -0400

    Updated JS

---
+++ icon-box-slider.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+   	$('.box-carousel').slick({
+		dots: false,
+		arrows: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		prevArrow: "<button type='button' class='mission-prev-arrow'></button>",
+		nextArrow: "<button type='button' class='mission-next-arrow'></button>"
+	});
+
+});
